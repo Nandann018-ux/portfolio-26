926df8607b34c93af659b3f88f46d10087b6c739
Nandan Acharya <samarthachar18@gmail.com>
2026-03-26 16:14:09 +0530

    refactor: remove standalone Spine component, now inline in page

--- src/components/Spine.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Spine() {
-  return (
-    <div id="spine" className="fixed left-1/2 top-0 bottom-0 w-[1px] bg-white pointer-events-none z-50">
-      {}
-    </div>
-  );
-}
