0a10ecfb205b4ed9ce66f31a0f754e79d99c5579
Nandan Acharya <samarthachar18@gmail.com>
2026-04-09 01:11:03 +0530

    chore: finalize comment removal for utility components

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -5,7 +5,6 @@ import Lenis from 'lenis';
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    // Prevent browser from restoring scroll position
     window.history.scrollRestoration = 'manual';
     window.scrollTo(0, 0);
 
@@ -18,7 +17,6 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
 
     requestAnimationFrame(raf);
 
-    // Initial scroll to top just in case
     lenis.scrollTo(0, { immediate: true });
 
     return () => {
